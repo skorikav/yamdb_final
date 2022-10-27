34906ae2a0bebd46d1fb9fd674f1ddc97ce1cecb
Анастасия Елина <skorikav@live.ru>
2022-10-27 11:27:25 +0300

    тест воркфлоу

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,9 +1,11 @@
 import os
 from datetime import timedelta
+from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
+env_path = Path('../infra')/'.env'
+load_dotenv(dotenv_path=env_path)
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
